ab2a34bd0a2ff744bf1a71e083c4fb39992dcc81
Raiza <wisdomadebola62@gmail.com>
2024-09-11 01:47:39 +0100

    chore: modify the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +28,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
-        {children}
+        <div className="relative flex min-h-screen flex-col pt-12 mx-auto">
+          {children}
+        </div>
+        <Toaster />
       </body>
     </html>
   );
