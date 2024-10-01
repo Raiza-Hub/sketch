afd1494fd77546e5d9e106cd30d9ebda1ed0c179
Raiza <wisdomadebola62@gmail.com>
2024-10-01 15:35:22 +0100

    deleted

--- src/lib/validators/createRoom.ts
+++
@@ -1,8 +0,0 @@
-import * as z from 'zod'
-
-export const createRoomSchema = z.object({
-  username: z
-    .string()
-    .min(6, 'Username must contain at least 2 characters')
-    .max(50, 'Username must not contain more than 50 characters'),
-});
--- src/lib/validators/joinRoom.ts
+++
@@ -1,9 +0,0 @@
-import * as z from 'zod'
-
-export const joinRoomSchema = z.object({
-  username: z
-    .string()
-    .min(6, 'Username must contain at least 2 characters')
-    .max(50, 'Username must not contain more than 50 characters'),
-  roomId: z.string().trim().length(21, 'Room ID must contain exactly 21 characters'),
-});
